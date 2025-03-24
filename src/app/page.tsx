e805d66d88aad381f71a20e459275946b624a13d
pasipasipada <irfanoputra27@gmail.com>
2025-03-25 00:39:11 +0700

    feat: add lanyard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import Lanyard from './components/Lanyard/Lanyard';
 
 export default function Home() {
   return (
-    <div className="min-h-screen overflow-x-hidden">
-      <div className="container border mx-auto h-screen">
-        <div className="grid grid-cols-12">
-          <div className="col-span-6">A</div>
-          <div className="col-span-6">B</div>
+    <div className="min-h-screen overflow-x-hidden bg-[#050a30]">
+      <div className="container mx-auto h-screen px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex justify-center items-center">
+            <div>A</div>
+          </div>
+          <div className="flex justify-center items-center">
+            <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
+          </div>
         </div>
       </div>
     </div>
